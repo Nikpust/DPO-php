42335f3b3c69ef16287a6678d848cb9ec4ab59c6
Nikita <20_nik_05@mail.ru>
2025-05-16 10:12:15 +0300

    fix(ProfileController.php): добавлен редирект на /login для неавторизованных пользователей

--- Задание 8. Booksy/src/controllers/ProfileController.php
+++ Задание 8. Booksy/src/controllers/ProfileController.php
@@ -10,9 +10,13 @@
     class ProfileController extends BaseController {
 
         public function index(): Response {
-            $model = new Book();
-            $books = $model->getByUserId($this->session->get('user_id'));
-            return new Response($this->twig->render('profile.twig', ['books' => $books]));
+            if ($this->session->get('user_id') == null) {
+                return new Response('', 302, ['Location' => '/login']);
+            } else {
+                $model = new Book();
+                $books = $model->getByUserId($this->session->get('user_id'));
+                return new Response($this->twig->render('profile.twig', ['books' => $books]));
+            }
         }
 
     }
