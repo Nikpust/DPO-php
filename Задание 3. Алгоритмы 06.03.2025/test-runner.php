f5c1e37d343f55dacf5ea444a4ead63b41f89faf
Nikita <20_nik_05@mail.ru>
2025-05-24 19:24:48 +0300

    docs(test-runner.php): добавлены комментарии к коду

--- Задание 3. Алгоритмы 06.03.2025/test-runner.php
+++ Задание 3. Алгоритмы 06.03.2025/test-runner.php
@@ -1,17 +1,20 @@
 <?php
-    $dir = readline("Проверяем [A/B/C/D]: ");
-    $path = __DIR__ . "/" . $dir;
-    $php_script = $path . "/" . $dir . ".php";
+    $dir = readline("Проверяем [A/B/C]: ");     # Запрашиваем у пользователя название подкаталога
+    $path = __DIR__ . "/" . $dir;               # Формируем абсолютный путь к подкаталогу
+    $php_script = $path . "/" . $dir . ".php";  # Путь к PHP-скрипту, который нужно проверить
 
+    # Перебираем все файлы с расширением .dat в выбранной директории
     foreach (glob("$path/*.dat") as $dat_file) {
-        $ans_file = str_replace(".dat", ".ans", $dat_file);
+        $ans_file = str_replace(".dat", ".ans", $dat_file);     # Для каждого .dat файла находим соответствующий .ans файл
 
-        $output = shell_exec("php \"$php_script\" < \"$dat_file\"");
-        $result = file_get_contents($ans_file);
-        
+        $output = shell_exec("php \"$php_script\" < \"$dat_file\"");    # Выполняем PHP-скрипт с входными данными из .dat файла
+        $result = file_get_contents($ans_file);                         # Читаем ожидаемый результат из .ans файла
+
+        # Удаляем лишние пробелы в конце строк
         $output = preg_replace('/\s+$/m', '', $output);
         $result = preg_replace('/\s+$/m', '', $result);
         
+        # Сравниваем фактический вывод скрипта и ожидаемый результат
         if ($dir != "C") {
             if ($output == $result) {
                 echo basename($dat_file) . " OK\n";
@@ -19,6 +22,7 @@
                 echo basename($dat_file) . " FAIL\n";
             }
         } else {
+            # Для задачи C сравниваем результат с погрешностью 0.0013 "ошибка округления в ответах"
             $output_exp = explode("\n", $output);
             $result_exp = explode("\n", $result);
 
